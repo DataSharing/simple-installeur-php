e0ab5269ed344a1e2df4c3b40317ac8b22faa4ef
DataSharing <datasharing7@gmail.com>
2016-06-06 11:51:31 +0200

    Projet
    Installeur PHP

---
+++ installation/index.php
@@ -0,0 +1,135 @@
+﻿<?php 
+include 'install.php'; 
+$install = New install();
+$sansmdp = '';
+$msgerror = '';
+$msgsucces = '';
+$code = -1;
+if(isset($_POST['sansmdp'])) $sansmdp = $_POST['sansmdp'];
+if(isset($_GET['code'])) $code = $_GET['code'];
+if(isset($_POST['suppr_rep_install'])) { $install->supprimer_rep_install();}
+if(isset($_POST['install'])){
+	if($sansmdp == 'on'){
+		if(!empty($_POST['hostname']) 
+                    && !empty($_POST['namebdd']) 
+                    && !empty($_POST['userbdd'])
+                    && !empty($_POST['mail'])
+                    && !empty($_POST['base_url'])
+                    && !empty($_POST['nom_du_site'])
+                    && !empty($_POST['password'])    
+                    && !empty($_POST['passwordconfirme'])){
+                    $msgerror = $install->index($_POST['prefixebdd'],
+                            $_POST['hostname'],
+                            $_POST['namebdd'],
+                            $_POST['userbdd'],
+                            NULL,
+                            $_POST['mail'],
+                            $_POST['base_url'],
+                            $_POST['nom_du_site'],
+                            $_POST['password'],
+                            $_POST['passwordconfirme']);
+		}
+	}elseif(!empty($_POST['hostname']) 
+                && !empty($_POST['namebdd']) 
+                && !empty($_POST['userbdd']) 
+                && !empty($_POST['pwdbdd'])
+                && !empty($_POST['mail'])
+                && !empty($_POST['base_url'])
+                && !empty($_POST['nom_du_site'])
+                && !empty($_POST['password'])    
+                && !empty($_POST['passwordconfirme'])){
+		$msgerror = $install->index($_POST['prefixebdd'],
+                        $_POST['hostname'],
+                        $_POST['namebdd'],
+                        $_POST['userbdd'],
+                        $_POST['pwdbdd'],
+                        $_POST['mail'],
+                        $_POST['base_url'],
+                        $_POST['nom_du_site'],
+                        $_POST['password'],
+                        $_POST['passwordconfirme']);
+	}else{
+		$msgerror = 'Tous les champs sont obligatoires!';
+	}
+}
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <?php if(isset($_POST['suppr_rep_install'])){echo '<meta http-equiv="Refresh" content="5; url=../index.php" />';}?>
+  <title>Installation ...</title>
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+<script>
+    function preload() {
+        document.getElementById("patienter").style.display = "block";
+    }
+</script>
+    <style>
+body {
+	padding:0;
+	margin:0;
+	background:#f1f1f1;
+}
+
+.container {
+	background:#fff;
+	border:1px solid #d3e0e9;
+	border-radius:5px;
+	padding:3em;
+	box-shadow:0 1px 8px #d3e0e9;
+	width:450px;
+}
+
+h3 {
+    margin:0 auto;
+}
+</style>
+</head>
+<body>
+<div class='container' style='margin:1em auto;width:450px'>
+<?php
+if($code == 0){
+	$install->InfosOk();
+}else{
+?>
+	<form action='' method='post'>
+            <h3><i>Installation app...</i></h3></br>   
+                <div class="progress" style="display:none" id="patienter">
+                    <div class="indeterminate"></div>
+                </div>
+		<?php 	if(!empty($msgerror)){ 
+					echo '<div class="card-panel red lighten-1" role="alert">';
+                                            echo '<span class="white-text text-darken-2">';
+                                                echo $msgerror;
+                                            echo "</span>";
+					echo '</div>';
+				}
+		?>
+                <input type="checkbox" name='sansmdp' id='sansmdp' /> 
+                <label for='sansmdp'>Hébergeur locaux sans mot de passe Mysql</label>
+                <h5>Infos site</h5>
+		<input type='text' name='nom_du_site' class='form-control' placeholder='Nom du site' value='<?php if(isset($_POST['nom_du_site'])) echo $_POST['nom_du_site'] ?>'/></br>
+		<input type='text' name='base_url' class='form-control' placeholder='exemple : http://localhost/site' value='<?php if(isset($_POST['base_url'])) echo $_POST['base_url'] ?>'/></br>
+                <input type='email' name='mail' class='form-control' placeholder='Adresse mail' value='<?php if(isset($_POST['mail'])) echo $_POST['mail'] ?>'/></br>
+                <input type='password' name='password' class='form-control' placeholder='mot de passe' /></br>
+                <input type='password' name='passwordconfirme' class='form-control' placeholder='Confirmer le mot de passe' /></br>
+		<h5>Base de données</h5>
+		<input type='text' name='hostname' class='form-control' placeholder='Nom du serveur' value='<?php if(isset($_POST['hostname'])) echo $_POST['hostname'] ?>'/></br>
+		<input type='text' name='namebdd' class='form-control' placeholder='Nom de la base de données' value='<?php if(isset($_POST['namebdd'])) echo $_POST['namebdd'] ?>'/></br>
+                <input type='text' name='prefixebdd' class='form-control' placeholder='Préfixe des tables' value='<?php if(isset($_POST['prefixebdd'])) echo $_POST['prefixebdd'] ?>'/></br>
+		<input type='text' name='userbdd' class='form-control' placeholder='Utilisateur' value='<?php if(isset($_POST['userbdd'])) echo $_POST['userbdd'] ?>'/></br>
+		<input type='password' name='pwdbdd' class='form-control' placeholder='Mot de passe' /></br>
+		<input type='submit' name='install' class='btn btn-success' onclick='preload()' style='width:100%' value='Installer' /></br>
+		
+
+	</form>
+<?php } ?>
+</div>
+</body>
+</html>
---
+++ installation/install.php
@@ -0,0 +1,171 @@
+﻿ <?php
+
+class Install {
+        protected $db;
+	public $msgerror;
+        public $msgsucces;
+        
+	function index($prefixebdd = NULL,$hostname,$namebdd,$userbdd,$pwdbdd,$mail,$base_url,$nom_du_site,$password,$passwordconfirme){
+            if($password == $passwordconfirme){
+		if($this->TestConnection($hostname,$namebdd,$userbdd,$pwdbdd)){
+                    if($this->CreateTableAndUser($mail,$password,$prefixebdd)){
+			if($this->CreateFileDatabase($prefixebdd,$hostname,$namebdd,$userbdd,$pwdbdd)){
+                            if($this->CreateFileConfigs($base_url,$nom_du_site)){
+                                header('location:?code=0');
+                            }else{
+                                return 'Le fichier de configuration n\'a pas été créé, veuillez vérifier les droits sur le repertoire ! (fichier : config.php)';
+                            }	
+			}else{
+                            return 'Le fichier de configuration n\'a pas été créé, veuillez vérifier les droits sur le repertoire ! (fichier : database.php)';
+			}
+                     }else{
+                         return 'Erreur à la création de l\'utilisateur!';
+                     }                       
+		}else{
+                    return $this->msgerror;
+		}
+            }else{
+                return 'Vous avez fait une erreur à la saisie du mot de passe de confirmation!';
+            }
+	}
+	
+	function CreateFileDatabase($prefixebdd = NULL,$hostname,$namebdd,$userbdd,$pwdbdd){
+		if(!is_dir('../config/')){
+			if (!mkdir('../config/', 0664, true)) {
+				die('Echec lors de la création des répertoires...');
+			}
+		}
+		$config = fopen('../config/database.php','w+');
+                fputs($config, "<?php".PHP_EOL);
+                fputs($config, '$database["prefixebdd"]= "'.$prefixebdd.'_";'.PHP_EOL);
+		fputs($config, '$database["hostname"]= "'.$hostname.'";'.PHP_EOL);
+                fputs($config, '$database["namebdd"]= "'.$namebdd.'";'.PHP_EOL);
+                fputs($config, '$database["userbdd"]= "'.$userbdd.'";'.PHP_EOL);
+                fputs($config, '$database["passbdd"]= "'.$pwdbdd.'";'.PHP_EOL);
+		fclose($config);
+		return true;
+	}
+        
+        function CreateFileConfigs($base_url,$nom_du_site){
+		$config = fopen('../config/config.php','w+');
+                fputs($config, "<?php".PHP_EOL);
+				fputs($config, '$config["date_du_jour"]= date("Y-m-d H:i:s");'.PHP_EOL);
+                fputs($config, '$config["nom_du_site"]= "'.$nom_du_site.'";'.PHP_EOL);
+				fputs($config, '$config["controller_principal"]= "Accueil";'.PHP_EOL);
+                fputs($config, '$config["base_url"]= "'.$base_url.'/";'.PHP_EOL);
+				fputs($config, '$config["langage"] = "fr-FR";'.PHP_EOL);
+		fclose($config);
+		return true;
+	}
+	
+        function CreateTableAndUser($mail,$password,$prefixebdd = NULL){
+			if(!$prefixebdd == NULL){
+				$prefixe = $prefixebdd."_";
+			}else{
+				$prefixe = "";
+			}
+
+            $create_table = "CREATE TABLE IF NOT EXISTS `" . $prefixe . "utilisateurs` (
+                              `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+							  `nom` VARCHAR(50) NULL COMMENT '',
+							  `prenom` VARCHAR(50) NULL COMMENT '',
+							  `telephone` VARCHAR(10) NULL COMMENT '',
+							  `ville` VARCHAR(150) NULL COMMENT '',
+							  `mail` VARCHAR(150) NULL COMMENT '',
+							  `password` VARCHAR(255) NULL COMMENT '',
+							  `salage` VARCHAR(255) NULL COMMENT '',
+							  `date_creation` DATE NULL COMMENT '',
+							  `active` INT(1) NULL DEFAULT '1',
+							  `corbeille` INT(1) NULL DEFAULT '0',
+							  PRIMARY KEY (`id`)  COMMENT '')
+							  ENGINE = InnoDB
+							  COMMENT = ''";
+			$create_table_logs = 'CREATE TABLE IF NOT EXISTS `' . $prefixe . 'logs` (
+									`id` int(11) NOT NULL AUTO_INCREMENT,
+									  `id_element` int(11) DEFAULT NULL,
+									  `controller` varchar(150) DEFAULT NULL,
+									  `modifie_par` varchar(150) DEFAULT NULL,
+									  `date_modification` datetime DEFAULT NULL,
+									  `action` varchar(255) DEFAULT NULL,
+									  PRIMARY KEY (`id`))';
+            $create_user = 'insert into '.$prefixe.'utilisateurs(nom,prenom,mail,password) values("Super","Admin","'.$mail.'","'.sha1($password).'")';
+            if($this->db->query($create_table) && $this->db->query($create_user) && $this->db->query($create_table_logs)){
+                return true;
+            }
+        }
+        
+	function TestConnection($hostname,$namebdd,$userbdd,$pwdbdd){
+		$DB = NULL;
+		Try {
+			$DB = new PDO("mysql:host=" . $hostname . ";dbname=" . $namebdd . "", "" . $userbdd . "", "" . $pwdbdd . "");
+			$DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->db = $DB;
+			return true;
+		} catch (PDOException $e) {
+			$this->msgerror = 'Échec lors de la connexion : ' . $e->getMessage();
+		}
+	}
+	
+	function InfosOk(){
+		echo '<div class="card-panel teal lighten-2" role="alert">';
+                echo '<span class="white-text text-darken-2">';
+		echo "Connexion à la base de données : <i class='material-icons dp48'>thumb_up</i></br>";
+                echo "Création du fichier database.php : <i class='material-icons dp48'>thumb_up</i></br>";
+                echo "Création du fichier de configuration.php : <i class='material-icons dp48'>thumb_up</i></br>";
+                echo "</span>";
+		echo "</div>";
+                echo "</br>";
+                echo "<p style='color:orange'><b>N'oubliez pas de supprimer le dossier d'installation</b></p>";
+                echo "<form action='' method='post'>";
+                echo "<div align='center' ><input type='submit' class='waves-effect waves-light btn orange";
+				if(isset($_POST['suppr_rep_install'])){
+					echo " disabled'";
+				}else{
+					echo "'";
+				}
+				echo "onclick='preload();' id='suppr_rep_install' name='suppr_rep_install' value='Supprimer' /></br></br>";
+                echo '<div class="preloader-wrapper active" id="patienter" style="display:none">';
+                echo '<div class="spinner-layer spinner-red-only">';
+                echo '<div class="circle-clipper left">';
+                echo '<div class="circle"></div>';
+                echo '</div><div class="gap-patch">';
+                echo '<div class="circle"></div>';
+                echo '</div><div class="circle-clipper right">';
+                echo '<div class="circle"></div>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div></div>';
+				if(isset($_POST['suppr_rep_install'])){
+					echo '<h5 style="color:orange;text-align:center"><b><i>DOSSIER SUPPRIME !</b></i></h5>';
+				}
+                echo "</form>";
+	}
+        
+        function supprimer_rep_install(){
+            $ex_url = explode('/', $_SERVER['REQUEST_URI']);
+            $dir = $_SERVER["DOCUMENT_ROOT"].$ex_url[1]."/".$ex_url[2];
+            if($this->deleteDir($dir)){
+               header('location:../index.php');
+            }
+        }
+        
+        public static function deleteDir($dirPath) {
+            if (! is_dir($dirPath)) {
+                throw new InvalidArgumentException("$dirPath must be a directory");
+            }
+            if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+                $dirPath .= '/';
+            }
+            $files = glob($dirPath . '*', GLOB_MARK);
+            foreach ($files as $file) {
+                if (is_dir($file)) {
+                    self::deleteDir($file);
+                } else {
+                    unlink($file);
+                }
+            }
+            rmdir($dirPath);
+        }
+}
+
+?>
